8e5f8a2149e4ba6a3f9442c643eea0512b470cba
dzoniradilica <dzonidzonatan2@gmail.com>
2024-06-10 21:42:19 +0200

    #

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import Searchbar from '../components/Searchbar';
 
 export default function HomePage() {
+  console.log('homepage');
+
   return (
     <div className="home-wrapper">
       <Searchbar />
