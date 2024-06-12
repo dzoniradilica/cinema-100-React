9ba22836ff8350053efabacd3b4733975decb1a7
dzoniradilica <dzonidzonatan2@gmail.com>
2024-06-12 23:32:48 +0200

    #

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -3,6 +3,8 @@ import { useLoaderData } from 'react-router';
 export default function Movies() {
   const movies = useLoaderData();
 
+  console.log('radiiii');
+
   return (
     <section className="movies">
       {movies.map(movie => {
