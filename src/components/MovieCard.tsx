0b676d44bdac9fd0df4cdba03b8b4366b3d57525
dzoniradilica <dzonidzonatan2@gmail.com>
2024-06-13 17:32:41 +0200

    Added style for movie card

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,16 +1,26 @@
 import { ConfigMovie } from '../configs/movie';
 
 export default function MovieCard({ movie }: { movie: ConfigMovie }) {
-  return (
-    <li key={movie.id} className="movie-card">
-      <img src={movie.image} alt={movie.title} />
-      <h3 className="movie-title">{movie.title}</h3>
+  console.log(movie.genre);
 
-      <div className="movie-info">
-        <span className="year">{movie.year}</span>
-        <span className="rating">{movie.rating}</span>
-        <span className="genre">{movie.genre}</span>
-      </div>
-    </li>
+  return (
+    <div className="col-md-3">
+      <li key={movie.id} className="movie-card">
+        <img src={movie.image} alt={movie.title} />
+        <h3 className="movie-title">{movie.title}</h3>
+
+        <div className="movie-info">
+          <span className="year">{movie.year}</span>
+          <span className="rating">{movie.rating}</span>
+          <span className="genre">
+            {movie.genre.length === 1
+              ? movie.genre
+              : movie.genre.map(mg => {
+                  return <span className="genre">{mg}</span>;
+                })}
+          </span>
+        </div>
+      </li>
+    </div>
   );
 }
